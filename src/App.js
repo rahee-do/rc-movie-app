5653fc044e95aa6fb4f5de99553b6d49b68e309a
soyeon100 <dev.dorahee100@gmail.com>
2022-10-15 20:40:54 +0900

    Component reuse example 1(with JSX + props)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+function Movie({title}) {
+  console.log(title)
+  return (
+      title === "happy" ? <h3>I love Movie!</h3> : <h3>I hate Movie!</h3>
+  )
+}
+function Food(props) {
+  console.log(props)
+  return props.favorite === "kimchi" ? <h3>I like kimchi and {props.like} !!!</h3> : <h3>I like {props.like} !!!</h3>
+}
 function App() {
   return (
     <div>
-      {"TEST"}
+      <h1>Hello!!!!</h1>
+      <Movie title={"happy"}/>
+      <Food like="bulgogi" favorite="kimchi" />
+      <Food like="ramen" />
+      <Food like="samgiopsal" />
+      <Food like="chukumi" />
+      <Food like="kimbap" />
     </div>
   );
 }
