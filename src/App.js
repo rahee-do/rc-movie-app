3a243273439a54eb056597147940fbc6db814810
soyeon100 <dev.dorahee100@gmail.com>
2022-10-20 21:55:18 +0900

    React.Component Class 라이프사이클(Lifecycle)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 // class component
 class App extends React.Component{
+    constructor(prop) {
+        super(prop);
+        console.log("hello");
+    }
     // class component 의 data 를 넣을 공간 (data is the will change.)
     state = {
         count: 0
@@ -13,8 +17,19 @@ class App extends React.Component{
         this.setState(current => ({count: current.count - 1}));
     };
 
+    componentDidMount() {
+        console.log("component rendered");
+    }
+    componentDidUpdate() {
+        console.log("I just updated");
+    }
+    componentWillUnmount() {
+        console.log("Goodbye cruel world");
+    }
+
     // class component 는 return 이 아닌 render 함수를 실행시킨다.
     render() {
+        console.log("I'm rendering");
         return (
             <div>
                 <h1>I'm a class component</h1>
