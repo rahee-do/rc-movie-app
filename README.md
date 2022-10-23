197978855a239310ffd3a4d9ada7072633b39a7a
soyeon100 <dev.dorahee100@gmail.com>
2022-10-23 19:45:25 +0900

    Movie Web service (React Class)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 React JS Fundamentals Course (2022 Update!)
 hello
+
+# [YTS Proxy](https://github.com/serranoarevalo/yts-proxy) 
+For the endpoint /list_movies.json use https://yts-proxy.now.sh/list_movies.json
--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+import "./Movie.css";
+
 // class component
 class App extends React.Component{
     // class component 의 data 를 넣을 공간 (data is the will change.)
@@ -6,17 +11,42 @@ class App extends React.Component{
         isLoading: true,
         movies: []
     };
+    getMovies = async () => {
+        // console.log(ES6 문법 : 구조분해?);
+        const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        console.log(movies);
+        this.setState({movies, isLoading: false});
+    }
     componentDidMount() {
-        setTimeout(() => {
-           this.setState({ isLoading: !this.state.isLoading, book: true })
-        }, 3000);
+        this.getMovies();
     };
 
     render() {
-        console.log("calss의 this.state를 사용하기 싫다면 ES6 문법을 사용하면 된다.");
-        const {isLoading} = this.state;
+        // console.log("calss의 this.state를 사용하기 싫다면 ES6 문법을 사용하면 된다.");
+        const {isLoading, movies} = this.state;
         return (
-            <div>{isLoading ? "Loading..." : "We are ready"}</div>
+            <section className="container">
+                {isLoading
+                    ? (
+                        <div className="loader">
+                            <span className="loader__text">Loading...</span>
+                        </div>
+                    ) : (
+                    <div className="movies">
+                        {movies.map(movie => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+            </section>
         );
     }
 }
